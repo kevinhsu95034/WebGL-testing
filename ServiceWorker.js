6ae3699a230b7e3798c4cbd658d01f266a857005
kevinhsu95034 <kevinhsu95034@gmail.com>
2022-08-02 13:25:46 +0800

    Updated control

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Kevin Hsu-Kevin's Immersive Portfolio-0.1";
+const cacheName = "Kevin Hsu-Kevin's Immersive Portfolio-0.2";
 const contentToCache = [
     "Build/WebTest.loader.js",
     "Build/WebTest.framework.js",
